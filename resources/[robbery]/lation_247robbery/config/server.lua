de56ba54a1672d26d7ec2c96e97a263265eb1423
B4RSKY <b4rskyy@gmail.com>
2025-08-17 14:33:14 +0700

    add robbery warung

---
+++ resources/[robbery]/lation_247robbery/config/server.lua
@@ -0,0 +1,36 @@
+return {
+
+    ----------------------------------------------
+    --        💬 Setup logging system
+    ----------------------------------------------
+
+    logs = {
+        -- What logging service do you want to use?
+        -- Available options: 'fivemanage', 'fivemerr', 'discord' & 'none'
+        -- It is highly recommended to use a proper logging service such as Fivemanage or Fivemerr
+        service = 'discord',
+        -- Do you want to include screenshots with your logs?
+        -- This is only applicable to Fivemanage and Fivemerr
+        screenshots = false,
+        -- You can enable (true) or disable (false) specific player events to log here
+        events = {
+            -- register_robbed is when a register has been robbed
+            register_robbed = false,
+            -- safe_robbed is when.. come on now, you gotta know..
+            safe_robbed = false
+        },
+        -- If service = 'discord', you can customize the webhook data here
+        -- If not using Discord, this section can be ignored
+        discord = {
+            -- The name of the webhook
+            name = 'LOGS Robbery Warung',
+            -- The webhook link
+            link = 'https://discord.com/api/webhooks/1406541255290257469/DBnyGzJXjwHYrKVBSgiVlo9gbzylXdG8BxhS1hKfCwybB7EtwlM4XWXoj7wyelFKdseC',
+            -- The webhook profile image
+            image = 'https://i.imgur.com/ILTkWBh.png',
+            -- The webhook footer image
+            footer = 'https://i.imgur.com/ILTkWBh.png'
+        }
+    }
+
+}
