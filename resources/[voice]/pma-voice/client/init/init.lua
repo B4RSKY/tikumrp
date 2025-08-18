ecded7c834c6fb2bb45c220b540a85c846ad38ac
B4RSKY <b4rskyy@gmail.com>
2025-08-18 21:20:37 +0700

    update pma voice

--- resources/[voice]/pma-voice/client/init/init.lua
+++ resources/[voice]/pma-voice/client/init/init.lua
@@ -48,3 +48,35 @@ AddEventHandler('onClientResourceStart', function(resource)
 
 	print('Script initialization finished.')
 end)
+
+local color = {
+	[1] = {r=143, g=255, b=75},
+	[2] = {r=75, g=203, b=255},
+	[3] = {r=255, g=75, b=149}
+}
+local alpha = 150
+local delay = 0
+local delayTime = 1000
+local seccess = nil
+
+local function voiceDistance(mode)
+    local distance = Cfg.voiceModes[mode][1] * 2
+    local r,g,b,a = color[mode].r, color[mode].g, color[mode].b, alpha
+    seccess = false
+    delay = delayTime
+    while not seccess do
+        DrawMarker(1, GetEntityCoords(PlayerPedId())+vector3(0,0,0), 0.0,0.0,0.0, 0.0,0.0,0.0, distance,distance,0.125, r,g,b,math.floor((delay*a)/delayTime), false,true,2,nil,nil,false)
+        DrawMarker(1, GetEntityCoords(PlayerPedId())+vector3(0,0,0.025), 0.0,0.0,0.0, 0.0,0.0,0.0, distance,distance,-0.125, r,g,b,math.floor((delay*a)/delayTime), false,true,2,nil,nil,false)
+        Wait(0)
+        delay = delay-10
+        if delay <= 0 then
+          seccess = true
+        end
+    end
+end
+
+AddEventHandler('pma-voice:setTalkingMode', function(mode)
+    seccess = true
+    Wait(10)
+    voiceDistance(mode)
+end)
