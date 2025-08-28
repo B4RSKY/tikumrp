7075350b83a500720687920fd934a1979441469a
B4RSKY <b4rskyy@gmail.com>
2025-08-28 14:52:02 +0700

    add garage police

--- resources/[data]/tk_utility/jobvehicle/shared/config.lua
+++ resources/[data]/tk_utility/jobvehicle/shared/config.lua
@@ -6,16 +6,131 @@ return {
             label = 'Dealer Polisi',
             ped = {
                 model = 'csb_cop',
-                coords = vector4(311.13, -1452.61, 29.97, 55.67)
+                coords = vector4(56.62, -385.04, 39.13, 84.53)
             },
-            spawn = vector4(297.23, -1440.6, 29.8, 229.49),
+            spawn = vector4(52.19, -383.5, 39.13, 70.58),
             vehicle = {
-                police = {
-                    price = 15000,
-                    label = 'KLX 150',
-                    prefixPlate = 'POL',
+                klx150 = {
+                    price = 15000,  --- Price for the police vehicle
+                    label = 'XLK 150',  --- Label for the vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
                     forRank = {
                         [0] = true, [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                triton = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Matsubishi Triton',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [0] = true, [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                evopol = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Matsubishi Evolution',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                    [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                rangerpol = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Furd Ranger',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                    [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                wolf1 = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'DAPC-WOLF',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                    [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                mazda6 = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Masda 6',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                psp_bmwgs = {
+                    price = 15000,  --- Price for the second police vehicle
+                    label = 'BMR R 1200 GS',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                goldpol = {
+                    price = 15000,  --- Price for the second police vehicle
+                    label = 'Hindo Goldwing',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                ['barracuda-j'] = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Barracuda',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                brimob1 = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Insurgent',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                firetruk = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Water Canon',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                ['bus-ir'] = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Bus Polisi',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+                jendral = {
+                    price = 25000,  --- Price for the second police vehicle
+                    label = 'Land Cruzer Jendral',  --- Label for the second vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [13] = true, [14] = true, [15] = true, [16] = true,
+                    }
+                },
+            },
+        },
+        {
+            job = 'police',
+            label = 'Dealer Helikopter Polisi',
+            ped = {
+                model = 'csb_cop',
+                coords = vec(82.88, -414.14, 55.33, 354.75)
+            },
+            spawn = vec(86.6, -404.99, 55.33, 350.7),
+            vehicle = {
+                polmav = {
+                    price = 200000,  --- Price for the police vehicle
+                    label = 'MAVERICK',  --- Label for the vehicle
+                    prefixPlate = 'POL',  --- Prefix for the vehicle plate
+                    forRank = {
+                        [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [16] = true
                     }
                 },
             }
