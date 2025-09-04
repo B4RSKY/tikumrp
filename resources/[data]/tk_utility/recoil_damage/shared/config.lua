b25c6c92de26ff78c76545051c782dad94c0da7c
B4RSKY <b4rskyy@gmail.com>
2025-09-04 14:26:08 +0700

    penyesuaian damage senjata

--- resources/[data]/tk_utility/recoil_damage/shared/config.lua
+++ resources/[data]/tk_utility/recoil_damage/shared/config.lua
@@ -1,18 +1,62 @@
 return {
-    [`WEAPON_PISTOL`] = {
-        damage = 0.1,
-        recoil = 4.5,
+    [`WEAPON_REVOLVER_MK2`] = {
+        damage = 0.6,
+        recoil = 1.5,
+        camShake = 1.5,
+        onShooting = function ()
+            if math.random() < 0.1 then
+                TriggerServerEvent('hud:server:GainStress', math.random(1, 2))
+            end
+        end
+    },
+    [`WEAPON_ASSSAULTRIFLE_MK2`] = {
+        damage = 0.4,
+        recoil = 0.15,
+        camShake = 0.3,
+        onShooting = function ()
+            if math.random() < 0.1 then
+                TriggerServerEvent('hud:server:GainStress', math.random(1, 2))
+            end
+        end
+    },
+    [`WEAPON_MICROSMG`] = {
+        damage = 0.8,
+        recoil = 0.3,
         camShake = 0.1,
         onShooting = function ()
-            -- if math.random() < 0.1 then
-            --     TriggerServerEvent('hud:server:GainStress', math.random(10, 20))
-            -- end
-            TriggerServerEvent('hud:server:GainStress', math.random(10, 20))
+            if math.random() < 0.1 then
+                TriggerServerEvent('hud:server:GainStress', math.random(1, 2))
+            end
+        end
+    },
+    [`WEAPON_MINISMG`] = {
+        damage = 0.2,
+        recoil = 0.1,
+        camShake = 0.1,
+        onShooting = function ()
+            if math.random() < 0.1 then
+                TriggerServerEvent('hud:server:GainStress', math.random(1, 2))
+            end
         end
     },
     [`WEAPON_PISTOL50`] = {
-        damage = 0.1,
+        damage = 0.5,
+        recoil = 0.3,
+        camShake = 0.08,
+        onShooting = function ()
+            if math.random() < 0.1 then
+                TriggerServerEvent('hud:server:GainStress', math.random(1, 2))
+            end
+        end
+    },
+    [`WEAPON_SNIPERRIFLE`] = {
+        damage = 3.5,
         recoil = 4.5,
-        camShake = 0.8,
+        camShake = 1.1,
+        onShooting = function ()
+            if math.random() < 0.1 then
+                TriggerServerEvent('hud:server:GainStress', math.random(1, 2))
+            end
+        end
     }
 }
