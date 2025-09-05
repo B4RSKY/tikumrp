059455c9b66c33dfc2fca97935ab84d30d2225c2
B4RSKY <b4rskyy@gmail.com>
2025-09-05 14:49:17 +0700

    Update crafting

--- resources/[data]/ox_inventory/data/crafting.lua
+++ resources/[data]/ox_inventory/data/crafting.lua
@@ -1,1 +1,163 @@
-return {}
+return {
+    {
+        name = 'crafting_armour',
+		items = {
+			{
+				name = 'armour',
+				ingredients = {
+					scrapmetal = 5,
+					WEAPON_HAMMER = 0.05
+				},
+				duration = 5000,
+				count = 2,
+			},
+		},
+		points = {},
+		zones = {
+			{
+				coords = vec3(900.51, -1152.77, 26.06),
+				size = vec3(3.8, 1.05, 0.15),
+				distance = 1.5,
+				rotation = 315.0,
+			},
+		},
+	},
+    {
+        name = 'crafting_senjata',
+		items = {
+			{
+				name = 'WEAPON_PISTOL50',
+				ingredients = {
+					copper = 50,
+					iron = 50,
+					alumunium = 55,
+					rubber = 40,
+					packaged_plank = 45,
+                    scrapmetal = 5,
+				},
+				duration = 60000,
+				count = 1,
+			},
+            {
+				name = 'WEAPON_REVOLVER_MK2',
+				ingredients = {
+					copper = 60,
+					iron = 55,
+                    alumunium = 65,
+                    rubber = 45,
+					packaged_plank = 50,
+                    scrapmetal = 5,
+				},
+				duration = 60000,
+				count = 1,
+			},
+            {
+				name = 'WEAPON_ASSAULTRIFLE_MK2',
+				ingredients = {
+					copper = 60,
+					iron = 55,
+                    alumunium = 65,
+                    rubber = 45,
+					packaged_plank = 50,
+                    scrapmetal = 5,
+				},
+				duration = 60000,
+				count = 1,
+			},
+            {
+				name = 'WEAPON_MICROSMG',
+				ingredients = {
+					copper = 60,
+					iron = 55,
+                    alumunium = 65,
+                    rubber = 45,
+					packaged_plank = 50,
+                    scrapmetal = 5,
+				},
+				duration = 60000,
+				count = 1,
+			},
+            {
+				name = 'WEAPON_MINISMG',
+				ingredients = {
+					copper = 60,
+					iron = 55,
+                    alumunium = 65,
+                    rubber = 45,
+					packaged_plank = 50,
+                    scrapmetal = 5,
+				},
+				duration = 60000,
+				count = 1,
+			},
+            {
+				name = 'WEAPON_SNIPERRIFLE',
+				ingredients = {
+					copper = 60,
+					iron = 55,
+                    alumunium = 65,
+                    rubber = 45,
+					packaged_plank = 50,
+                    scrapmetal = 5,
+				},
+				duration = 60000,
+				count = 1,
+			},
+		},
+		points = {},
+		zones = {
+			{
+				coords = vec3(5196.23, -5133.30, 3.37),
+				size = vec3(3.8, 1.05, 0.15),
+				distance = 1.5,
+				rotation = 315.0,
+			},
+		},
+	},
+    {
+        name = 'crafting_toolsrampok',
+		items = {
+			{
+				name = 'lockpick',
+				ingredients = {
+					scrapmetal = 5,
+					WEAPON_HAMMER = 0.05
+				},
+				duration = 5000,
+				count = 2,
+			},
+		},
+		points = {},
+		zones = {
+			{
+				coords = vec3(279.70, 6782.44, 15.70),
+				size = vec3(3.8, 1.05, 0.15),
+				distance = 1.5,
+				rotation = 315.0,
+			},
+		},
+	},
+    {
+        name = 'crafting_ammo',
+		items = {
+			{
+				name = 'lockpick',
+				ingredients = {
+					scrapmetal = 5,
+					WEAPON_HAMMER = 0.05
+				},
+				duration = 5000,
+				count = 2,
+			},
+		},
+		points = {},
+		zones = {
+			{
+				coords = vec3(1832.08, -1182.70, 91.57),
+				size = vec3(3.8, 1.05, 0.15),
+				distance = 1.5,
+				rotation = 315.0,
+			},
+		},
+	},
+}
